ef8aa41a8c365d25d80c042251f777d4bf74a2aa
jelbom <jelbom@homeaway.com>
2021-01-14 12:36:32 -0600

    config clean-up and script update

--- components/Item/Item.js
+++ components/Item/Item.js
@@ -4,7 +4,6 @@ import {CSSTransition} from 'react-transition-group';
 import styles from './Item.module.scss'
 
 export default function Item({id, type, name, birthday, image, sortKey, index}) {
-    const [isOver, setIsOver] = useState(false);
     const makeKey = () => `item-${Math.random().toString(16).slice(2)}`;
     const prevSortKey = useRef(sortKey);
     const sortChanged = sortKey !== prevSortKey.current;
@@ -15,13 +14,14 @@ export default function Item({id, type, name, birthday, image, sortKey, index}) 
     const innerKey = useRef(makeKey())
     const formattedDate = useMemo(() => new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: 'numeric' }).format(new Date(birthday.replace(/-/gi, '/'))
     , [birthday]));
-    if (sortChanged && indexChanged) {
+    const hasChanged = sortChanged || indexChanged // sortChanged && indexChanged
+    if (sortChanged || indexChanged) {
         innerKey.current = makeKey();
     }
     return (
         <li className={classnames(styles.item, 'item')}>
             <div
-                className={classnames(styles.item_inner, {[styles.item_inner_sort]: sortChanged && indexChanged})}
+                className={classnames(styles.item_inner, {[styles.item_inner_sort]: hasChanged})}
                 key={innerKey.current}
             >
                 <div className={styles.image} style={{backgroundImage: `url('${image}')`}}>
